12f28ae144a5088a68960a3cb961b6a1bfe667e0
郭鹏 <guopeng@qiushibaike.com>
2018-09-13 19:08:40 +0800

    feat: 新增获取移动轨迹方法

--- src/touch.js
+++ src/touch.js
@@ -1,4 +1,10 @@
 /**
+ * TODO:
+ * 1、事件委托
+ * 2、多个手指测试
+ */
+
+/**
  * 1. 创建自定义事件实例 new Event('tap)
  * 2. 添加事件 element.addEventListener
  * 3. 发布事件 element.dispatchEvent
@@ -6,6 +12,9 @@
  * Init的时候把touchstart等事件都绑进去，然后存下来on了哪些touch事件，off的时候先删除存的事件，全部删除之后就解绑touchstart等事件
  * 那就不需要事件的类了，touch里面只保留对事件触发的判断
  */
+
+// TODO: 有一个方法，这个方法会返回用户的手指的轨迹数学表达，比如，开始到结束移动距离和角度，开始到中间移动距离和角度，中间到结束移动距离和角度
+// direction { up, down, left, right}
 
 const touch = function (element) {
     return new touch.prototype.Init(element);
@@ -20,6 +29,7 @@ touch.prototype = {
         SWIPE: 'swipe',
         PINCH: 'pinch'
     },
+    _originalEventInfo: {},
     Init: function (element) {
         this.element = element;
         this._bindedEventList = [];
@@ -37,7 +47,7 @@ touch.prototype = {
         };
     },
     _updateOriginalEventInfo: function (originalEventName, e) {
-        console.log(e);
+        console.log(originalEventName, e);
         const isTouchMove = originalEventName === 'touchmove';
         const eventInfo = {
             timeStamp: e.timeStamp,
@@ -66,34 +76,106 @@ touch.prototype = {
         this.element.addEventListener('touchend', (e) => {
             this._updateOriginalEventInfo('touchend', e);
 
-            if (this._canTriggerTapEvent()) {
+            if (this._canTriggerCustomEvent(tapEventName)) {
                 this.element.dispatchEvent(this._getEventInstance(tapEventName));
             }
         });
     },
-    _canTriggerTapEvent: function () {
-        const tapEventName = this._EVENT_NAME_MAP.TAP;
-        return this._getEventInstance(tapEventName) && this._isBelongTapEvent();
-    },
-    _isBelongTapEvent: function () {
-        // TODO: 最大的值怎么得出来？
-        const maxDelay = 300; // ms
-        const maxOffset = 16;
-        const info = this._originalEventInfo;
-        const l = info.touchend.changedTouches.length;
-        const offsetX = info.touchend.changedTouches[l - 1].screenX - info.touchstart.touches[0].screenX;
-        const offsetY = info.touchend.changedTouches[l - 1].screenY - info.touchstart.touches[0].screenY;
+    /**
+     * @range {string} all, firstHalf, secondHalf
+     */
+    _getMotion: function (range = 'all') {
+        const f = this._originalEventInfo;
+        const moves = [f.touchstart].concat(f.touchmove).concat([f.touchend]);
+        const firstIndex = 0;
+        const halfIndex = Math.floor(moves.length / 2);
+        const lastIndex = moves.length - 1;
+        const indexMap = {
+            all: [firstIndex, lastIndex],
+            firstHalf: [firstIndex, halfIndex],
+            secondHalf: [halfIndex, lastIndex]
+        };
+        const move_begin = moves[indexMap[range][0]];
+        const move_end = moves[indexMap[range][1]];
 
-        const isLessMaxDelay = info.touchend.timeStamp - info.touchstart.timeStamp < maxDelay;
-        const isLessMaxOffset = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)) < maxOffset;
+        const dt = move_end.timeStamp - move_begin.timeStamp;
+        const dx = move_end.screenX - move_begin.screenX;
+        // 坐标系更改成左下角
+        const dy = (window.screen.height - move_end.screenY) - (window.screen.height - move_begin.screenY);
+        const dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
-        return isLessMaxDelay && isLessMaxOffset;
+        const duration = dt;
+        const distance = dist;
+        let angle = 180 * (Math.atan2(dy, dx)) / Math.PI;
+        let direction = '';
+
+        // 一象限
+        if (dx > 0 && dy > 0) {
+            if (angle <= 45) {
+                direction = 'right';
+            }
+            else {
+                angle = 90 - angle;
+                direction = 'up';
+            }
+        }
+        // 二象限
+        if (dx < 0 && dy > 0) {
+            if (angle <= 135) {
+                angle = angle - 90;
+                direction = 'up';
+            }
+            else {
+                angle = 180 - angle;
+                direction = 'left';
+            }
+        }
+        // 三象限
+        if (dx < 0 && dy < 0) {
+            if (angle <= -135) {
+                angle = 180 - Math.abs(angle);
+                direction = 'left';
+            }
+            else {
+                angle = Math.abs(angle) - 90;
+                direction = 'down';
+            }
+        }
+        // 四象限
+        if (dx > 0 && dy < 0) {
+            if (angle <= -45) {
+                angle = 90 - Math.abs(angle);
+                direction = 'down';
+            }
+            else {
+                angle = Math.abs(angle);
+                direction = 'right';
+            }
+        }
+
+        return {
+            duration,
+            direction,
+            distance,
+            angle
+        };
     },
-    _canTriggerPressEvent: function () {
-        const pressEventName = this._EVENT_NAME_MAP.PRESS;
-        return this._getEventInstance(pressEventName) && this._isBelongPressEvent();
+    _isBelongCustomEvent: function (eventName) {
+        let isBelong;
+
+        switch (eventName) {
+            case this._EVENT_NAME_MAP.TAP: {
+                const motion = this._getMotion();
+                // TODO: 最大的值怎么得出来？
+                isBelong = motion.duration < 300 && motion.distance < 16;
+                break;
+            }
+        }
+
+        return isBelong;
     },
-    _isBelongPressEvent: function () {
+    _canTriggerCustomEvent: function (eventName) {
+        return this._getEventInstance(eventName) && this._isBelongCustomEvent(eventName);
     },
     _getBindedEventListItem: function (eventName) {
         return this._bindedEventList.find((item) => {
